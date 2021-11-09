2cdece43600393bd31e78a07ffcd6ba7042751f5
Antony Le Bechec <antony.lebechec@chru-strasbourg.fr>
2021-11-09 16:31:10 +0100

    V20211109

--- config/rules/variants.filtration.rules.mk
+++ config/rules/variants.filtration.rules.mk
@@ -4,27 +4,31 @@
 # Date: 25/05/2021
 # Author: Antony Le Bechec
 ############################
-MK_RELEASE="0.9.0.1"
-MK_DATE="25/05/2021"
+MK_RELEASE="0.9.1.0"
+MK_DATE="09/11/2021"
 
 # Release notes:
 # 0.9.0.0-14/10/2019: Creation
 # 0.9.0.1-25/05/2021: Recheck Expressions
+# 0.9.1.0-09/11/2021: Expression variables
 
 
 #####################
 # VariantFiltration #
 #####################
 
-filterExpression=--filterExpression "MQ0 >= 4 && ((MQ0 / (1.0 * DP)) > 0.1)" --filterName "HARD_TO_VALIDATE" --filterExpression "DP == 0" --filterName "VeryVeryLowDepth" --filterExpression "DP > 0 && DP < 10" --filterName "VeryLowDepth" --filterExpression "DP >= 10 && DP < 30" --filterName "LowDepth" --filterExpression "QUAL == 0" --filterName "VeryVeryLowQual" --filterExpression "QUAL > 0 && QUAL < 30.0" --filterName "VeryLowQual" --filterExpression "QUAL >= 30.0 && QUAL < 50.0" --filterName "LowQual" --filterExpression "QD >= 0.0 && QD < 1.5" --filterName "LowQD"
-genotypeFilterExpression=--genotypeFilterExpression "GQ == 0" --genotypeFilterName "VeryVeryLowGQ"  --genotypeFilterExpression "GQ > 0 && GQ < 50.0" --genotypeFilterName "VeryLowGQ"  --genotypeFilterExpression "GQ >= 50.0 && GQ < 90.0" --genotypeFilterName "LowGQ" --genotypeFilterExpression "DP == 0" --genotypeFilterName "VeryVeryLowDP" --genotypeFilterExpression "DP >= 0 && DP < 10" --genotypeFilterName "VeryLowDP"  --genotypeFilterExpression "DP >= 10 && DP < 30" --genotypeFilterName "LowDP"
+
+# VARIABLES
+
+FILTRATION_VCF_FILTER_EXPRESSION?=--filterExpression "MQ0 >= 4 && ((MQ0 / (1.0 * DP)) > 0.1)" --filterName "HARD_TO_VALIDATE" --filterExpression "DP == 0" --filterName "VeryVeryLowDepth" --filterExpression "DP > 0 && DP < 10" --filterName "VeryLowDepth" --filterExpression "DP >= 10 && DP < 30" --filterName "LowDepth" --filterExpression "QUAL == 0" --filterName "VeryVeryLowQual" --filterExpression "QUAL > 0 && QUAL < 30.0" --filterName "VeryLowQual" --filterExpression "QUAL >= 30.0 && QUAL < 50.0" --filterName "LowQual" --filterExpression "QD >= 0.0 && QD < 1.5" --filterName "LowQD"
+FILTRATION_VCF_GENOTYPE_FILTER_EXPRESSION?=--genotypeFilterExpression "GQ == 0" --genotypeFilterName "VeryVeryLowGQ"  --genotypeFilterExpression "GQ > 0 && GQ < 50.0" --genotypeFilterName "VeryLowGQ"  --genotypeFilterExpression "GQ >= 50.0 && GQ < 90.0" --genotypeFilterName "LowGQ" --genotypeFilterExpression "DP == 0" --genotypeFilterName "VeryVeryLowDP" --genotypeFilterExpression "DP >= 0 && DP < 10" --genotypeFilterName "VeryLowDP"  --genotypeFilterExpression "DP >= 10 && DP < 30" --genotypeFilterName "LowDP"
+
+FILTRATION_VCF_INVALIDATE_PREVIOUS_FILTERS_OPTION?=$(shell if (( $(FILTRATION_VCF_INVALIDATE_PREVIOUS_FILTERS) )); then echo " --invalidatePreviousFilters "; fi )
 
 
-#--filterExpression "QUAL < 30.0 && QUAL > 0 " --filterName "VeryLowQual" --filterExpression "QUAL == 0 " --filterName "VeryVeryLowQual"
 
 %.vcf: %.filtration.vcf %.filtration.vcf.idx %.empty.vcf %.genome
-	#$(BCFTOOLS) annotate -x FILTER $< > $@.filter_removed.vcf
-	$(JAVA) $(JAVA_FLAGS) -jar $(GATK) -T VariantFiltration -R $$(cat $*.genome) --variant $< -o $@ --invalidatePreviousFilters $(filterExpression) $(genotypeFilterExpression)
+	$(JAVA) $(JAVA_FLAGS) -jar $(GATK) -T VariantFiltration -R $$(cat $*.genome) --variant $< -o $@ $(FILTRATION_VCF_INVALIDATE_PREVIOUS_FILTERS_OPTION) $(FILTRATION_VCF_FILTER_EXPRESSION) $(FILTRATION_VCF_GENOTYPE_FILTER_EXPRESSION)
 	if [ ! -e $@ ]; then cp $*.empty.vcf $@; fi;
 	if [ ! -e $@ ]; then touch $@; fi;
 	rm -f $<.idx $@.idx $*.filtration.vcf*
