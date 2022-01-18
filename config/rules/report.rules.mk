c49748961678bdd8951a987ea55119cbd38a2ece
lebechec <antony.lebechec@chru-strasbourg.fr>
2022-01-18 18:33:56 +0100

    Report run variants merge - fix genotype

--- config/rules/report.rules.mk
+++ config/rules/report.rules.mk
@@ -230,7 +230,7 @@ REPORT_SECTIONS?=ALL
 %.variants: $(VCF_REPORT_FILES) %.variants_full #$(REPORT_FILES)
 	# List of final VCF files
 	echo $^ | tr " " "\n" | tr "\t" "\n" | grep "final.vcf.gz$$" > $@.tmp.vcf_list
-	$(BCFTOOLS) merge -l $@.tmp.vcf_list > $@.tmp.merged;
+	$(BCFTOOLS) merge -l $@.tmp.vcf_list | $(BCFTOOLS) filter -S . -e 'GT=="0/0" | GT=="0|0"' > $@.tmp.merged;
 	# Annotation
 	+$(HOWARD) $(HOWARD_CONFIG_OPTIONS) --input=$@.tmp.merged --output=$@.tmp.annotated1 --annotation=$(HOWARD_ANNOTATION_ANALYSIS) --threads=$(THREADS);
 	# Annotation dejavu (forced)
@@ -265,9 +265,9 @@ REPORT_SECTIONS?=ALL
 			cat $$(echo $$List_of_genes_files) | $(BEDTOOLS) sort | $(BEDTOOLS) merge > $@.tmp.GENES.$$genes_file; \
 			# Generate VCF Panel from VCF Design (especially $@.tmp.calculated.prioritized.sorted.vcf.gz because tabix) with $$genes_file for List of Samples \
 			if (( $$(grep ^ $@.tmp.GENES.$$genes_file -c) )); then \
-				$(BCFTOOLS) view --samples $$List_of_samples --force-samples $@.tmp.calculated.prioritized.sorted.vcf.gz -R $@.tmp.GENES.$$genes_file > $@.Panel.$$genes_file.vcf; \
+				$(BCFTOOLS) view --samples $$List_of_samples -U --force-samples $@.tmp.calculated.prioritized.sorted.vcf.gz -R $@.tmp.GENES.$$genes_file > $@.Panel.$$genes_file.vcf; \
 			else \
-				$(BCFTOOLS) view --samples $$List_of_samples --force-samples $@.tmp.calculated.prioritized.sorted.vcf.gz > $@.Panel.$$genes_file.vcf; \
+				$(BCFTOOLS) view --samples $$List_of_samples -U --force-samples $@.tmp.calculated.prioritized.sorted.vcf.gz > $@.Panel.$$genes_file.vcf; \
 			fi; \
 			# Compress VCF \
 			$(BGZIP) $@.Panel.$$genes_file.vcf; \
@@ -286,7 +286,7 @@ REPORT_SECTIONS?=ALL
 		# List of full VCF files \
 		echo $^ | tr " " "\n" | tr "\t" "\n" | grep "full.vcf.gz$$" > $@.tmp.vcf_list; \
 		for f in $$(cat $@.tmp.vcf_list); do $(BCFTOOLS) view -h $$f | grep "^#CHROM" | cut -f10- | sed  "s/^/$$(basename $$f | cut -d. -f1)./g;s/\t/\t$$(basename $$f | cut -d. -f1)./g";  done | tr "\t" "\n" > $@.tmp.vcf_list.pipelines; \
-		$(BCFTOOLS) merge -l $@.tmp.vcf_list --force-samples | $(BCFTOOLS) reheader -s $@.tmp.vcf_list.pipelines > $@.tmp.merged; \
+		$(BCFTOOLS) merge -l $@.tmp.vcf_list --force-samples | $(BCFTOOLS) filter -S . -e 'GT=="0/0" | GT=="0|0"' | $(BCFTOOLS) reheader -s $@.tmp.vcf_list.pipelines > $@.tmp.merged; \
 		# Annotation; \
 		$(HOWARD) $(HOWARD_CONFIG_OPTIONS) --input=$@.tmp.merged --output=$@.tmp.annotated0 --annotation=$(HOWARD_ANNOTATION_ANALYSIS) --norm=$$(cat $*.genome) --threads=$(THREADS); \
 		# Annotation dejavu (forced); \
@@ -321,9 +321,9 @@ REPORT_SECTIONS?=ALL
 				cat $$(echo $$List_of_genes_files) | $(BEDTOOLS) sort | $(BEDTOOLS) merge > $@.tmp.GENES.$$genes_file; \
 				# Generate VCF Panel from VCF Design (especially $@.tmp.calculated.prioritized.sorted.vcf.gz because tabix) with $$genes_file for List of Samples \
 				if (( $$(grep ^ $@.tmp.GENES.$$genes_file -c) )); then \
-					$(BCFTOOLS) view --force-samples $@.tmp.calculated.prioritized.sorted.vcf.gz -R $@.tmp.GENES.$$genes_file > $@.Panel.$$genes_file.vcf; \
+					$(BCFTOOLS) view --force-samples -U $@.tmp.calculated.prioritized.sorted.vcf.gz -R $@.tmp.GENES.$$genes_file > $@.Panel.$$genes_file.vcf; \
 				else \
-					$(BCFTOOLS) view --force-samples $@.tmp.calculated.prioritized.sorted.vcf.gz > $@.Panel.$$genes_file.vcf; \
+					$(BCFTOOLS) view --force-samples -U $@.tmp.calculated.prioritized.sorted.vcf.gz > $@.Panel.$$genes_file.vcf; \
 				fi; \
 				# Compress VCF \
 				$(BGZIP) $@.Panel.$$genes_file.vcf; \
