72bbff9da9287781ea441d4d8f96da40e7068640
lebechec <antony.lebechec@chru-strasbourg.fr>
2022-01-19 15:41:42 +0100

    Change UMI options in MarkDuplicates rule

--- config/rules/markduplicates.rules.mk
+++ config/rules/markduplicates.rules.mk
@@ -3,11 +3,12 @@
 # Author: PACHCHEK Sinthuja and SARAROLS Pauline
 ############################
 # Release
-MK_RELEASE="0.9.1beta"
-MK_DATE="06/09/2016"
+MK_RELEASE="0.9.2.0"
+MK_DATE="19/01/2022"
 
 ## Release note
 # 25/07/2014: DEV into PROD. Creation V0.9. Splitting clipping. Option file from BED file. Checking Option file empty. Merge resulting SAM
+# 19/01/2022: Changing UMI options
 
 
 BARCODE_TAG?=
@@ -26,7 +27,7 @@ BARCODE_TAG?=
 		O=$@ \
 		M=$<.metrics/$(*F).markDuplicates.metrics \
 		$$(if [ "$(BARCODE_TAG)" != "" ]; then echo "BARCODE_TAG=$(BARCODE_TAG)"; fi;) \
-		$$(if [ "$(UMI_TYPE)" == "duplex" ]; then echo "DUPLEX_UMI=1"; fi;) \
+		$$(if [ "$(UMI_TYPE)" == "duplex" ]; then echo "DUPLEX_UMI=True"; fi;) \
 		VALIDATION_STRINGENCY=SILENT COMPRESSION_LEVEL=1 TMP_DIR=$(TMP_FOLDER_TMP) \
 		$(PICARD_MARKDUP_OPTICAL_DEDUP) \
 		1>$<.metrics/$(*F).markDuplicates.metrics.log 2>$<.metrics/$(*F).markDuplicates.metrics.err  || (echo "WARNING : picard.jar MarkDuplicates failed !"); \
@@ -39,7 +40,7 @@ BARCODE_TAG?=
 
 
 
-RELEASE_COMMENT := "\#\# MARK_DUPLICATES: Mark duplicated reads in BAM with PICARD MarkDuplicates. Not parallelized"
+RELEASE_COMMENT := "\#\# MARK_DUPLICATES: Mark duplicated reads in BAM with PICARD MarkDuplicates. UMI options. Not parallelized"
 RELEASE_CMD := $(shell echo "$(RELEASE_COMMENT)" >> $(RELEASE_INFOS) )
 
 PIPELINES_COMMENT := "POST_ALIGNMENT:markduplicates:Mark duplicated reads in BAM with PICARD MarkDuplicates. Use BARCODE_TAG to specify tag"
