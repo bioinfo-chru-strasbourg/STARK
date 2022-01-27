6e06a6e5d9c1684a32fb299bbb86c4c4805b438f
lebechec <antony.lebechec@chru-strasbourg.fr>
2022-01-27 16:38:53 +0100

    Fix dejavu script - fix error in dejavu generation

--- toolbox/dejavu.generate.sh
+++ toolbox/dejavu.generate.sh
@@ -7,8 +7,8 @@
 
 SCRIPT_NAME="STARK_DEJAVU"
 SCRIPT_DESCRIPTION="STARK DEJAVU ANNOVAR databases generation"
-SCRIPT_RELEASE="0.12.2"
-SCRIPT_DATE="29/10/2021"
+SCRIPT_RELEASE="0.12.3"
+SCRIPT_DATE="27/01/2022"
 SCRIPT_AUTHOR="Antony Le Bechec"
 SCRIPT_COPYRIGHT="HUS"
 SCRIPT_LICENCE="GNU-AGPL"
@@ -22,6 +22,7 @@ RELEASE_NOTES=$RELEASE_NOTES"# 0.11.0-29/09/2020: Add STARK module json files.\n
 RELEASE_NOTES=$RELEASE_NOTES"# 0.12.0-17/12/2020: Add sample filter parameters.\n";
 RELEASE_NOTES=$RELEASE_NOTES"# 0.12.1-11/01/2021: Add group project filter, annotation, calculation and nomen fields parameters, rebuld search goup/project folders.\n";
 RELEASE_NOTES=$RELEASE_NOTES"# 0.12.2-29/10/2021: Fix error in input vcf.\n";
+RELEASE_NOTES=$RELEASE_NOTES"# 0.12.3-27/01/2022: Fix error dejavu generation.\n";
 
 # Script folder
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
