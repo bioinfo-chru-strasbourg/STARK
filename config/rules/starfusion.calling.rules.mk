c4fdd8b5f8ce4823a921cb165b43664eec91a79c
sam ncs <ichtyornis@gmail.com>
2022-09-07 19:11:06 +0200

    working STARFusion rule

--- config/rules/starfusion.calling.rules.mk
+++ config/rules/starfusion.calling.rules.mk
@@ -8,7 +8,7 @@
 %.STARFusion$(POST_CALLING).vcf: %.bam %.bam.bai %.empty.vcf %.genome %.junction
 	mkdir -p $*.fusion.reports;
 	STAR-Fusion --chimeric_junction $*.junction --genome_lib_dir $$(cat $*.genome | xargs -0 dirname) --output_dir $*.fusion.reports;
-	variantconvert convert -i $*.fusion.reports/star-fusion.fusion_predictions.tsv -o $@ -fi breakpoints -fo vcf -c /STARK/data/variantconvert/configs/config_starfusion.json;
+	variantconvert convert -i $*.fusion.reports/star-fusion.fusion_predictions.tsv -o $@ -fi breakpoints -fo vcf -c /STARK/data/variantconvert/configs/config_starfusion_bas.json;
 
 # CONFIG/RELEASE
 RELEASE_COMMENT := "\#\# CALLING STARFusion '$(MK_RELEASE)': CTAT Tool to detect fusions based on RNA-Seq data"
