de723d6a264d1bdec5d5ea3bf48c4672b47d95e8
Antony Le Béchec <antony.lebechec@gmail.com>
2026-05-19 22:19:58 +0200

    Security on json params from users, docker exec mode improve

--- services/stark/stark/api/dockerfile/app/security.py
+++ services/stark/stark/api/dockerfile/app/security.py
@@ -60,10 +60,12 @@ _FORBIDDEN_DOCKER_FLAGS = {
 }
 
 # Keys that are mandatory in the module config for security reasons and will be filled with safe defaults if missing/None in the config file. The presence of the mandatory keys is crucial for ensuring that each module has a defined Docker image, service name, and container name, which are essential for the secure operation of the analysis execution. The mandatory params will be filled with safe defaults to ensure secure operation even if the module is misconfigured or missing these fields.
-MANDATORY_KEYS_PARAMS = ("image", "service", "container")
+MANDATORY_KEYS_PARAMS = ("module", "image", "service", "container")
 MANDATORY_PARAMS = {
     "docker_extra_params": "",
     "use_stark_container_mount": False,
+    "command_prefix": "",
+    "command_postfix": "",
 }
 # Sensitive keys include both the mandatory keys that must be present in the module config for security reasons, and the mandatory params that will be filled with safe defaults if missing to ensure secure operation even with misconfigured modules.
 SENSITIVE_KEYS = MANDATORY_KEYS_PARAMS + tuple(MANDATORY_PARAMS.keys())
